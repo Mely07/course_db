ca86d0bdc45115888885e3cd2701006059001e65
Mely07 <damely.tineo07@gmail.com>
2021-12-03 12:57:20 -0500

    Set up show method to display JSON data for a specific course using an id. Course defined within callback, to be shared among various methods.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,7 +1,18 @@
 class CoursesController < ApplicationController
+    before_action :set_course, only: [:show]
+
     def index 
         courses = Course.all
         render json: courses
+    end
+
+    def show 
+        render json: @course
+    end
+
+    private
+    def set_course
+        @course = Course.find_by(id: params[:id])
     end
 
 end
