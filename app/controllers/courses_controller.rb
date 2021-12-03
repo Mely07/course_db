5e9444900271029bc3ddc3f861f1ca503bb8bdeb
Mely07 <damely.tineo07@gmail.com>
2021-12-03 13:50:18 -0500

    Set up method to create new course. Added additional course validation to ensure proper date format.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -10,9 +10,23 @@ class CoursesController < ApplicationController
         render json: @course
     end
 
+    def create 
+        course = Course.new(course_params)
+
+        if course.save 
+            render json: course, status: :created
+        else
+            render json: course.errors, status: :unprocessable_entity
+        end
+    end
+
     private
     def set_course
         @course = Course.find_by(id: params[:id])
+    end
+
+    def course_params
+        params.require(:course).permit(:name, :release_date)
     end
 
 end
